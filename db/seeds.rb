4751b909242771036e2d3220b629aecd55974ba2
adinareiter <144619417+adinareiter@users.noreply.github.com>
2023-10-31 13:48:30 -0400

    added 3 more image data sets through seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,20 @@
 #   { name: "t-shirt", price: "20.0", image_url: "https://media.istockphoto.com/id/1347667305/photo/blank-sweatshirt-mock-up-in-front-and-back-views.webp?b=1&s=170667a&w=0&k=20&c=NZW8dm1yB2iZMmapyNG3qT1d-lokMwpHXHVBkRJmnhg=", description: "long sleeved crew neck t-shirt black", stock: 50, supplier_id: 1 },
 # ])
 
+# image = Image.create({
+#   url: "https://images.unsplash.com/photo-1603487742131-4160ec999306?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2xpcHBlcnN8ZW58MHx8MHx8fDA%3D",
+#   clothing_id: 1,
+# })
+
 image = Image.create({
-  url: "https://images.unsplash.com/photo-1603487742131-4160ec999306?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8c2xpcHBlcnN8ZW58MHx8MHx8fDA%3D",
-  clothing_id: 1,
+  url: "https://media.istockphoto.com/id/1329845318/photo/brown-winter-down-jacket-front-and-back-view.webp?b=1&s=170667a&w=0&k=20&c=scug3grBM9jeGFFGEmm7fy5443qyPGsgz6qbjEQU_AE=",
+  clothing_id: 2,
+})
+image = Image.create({
+  url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKy5QX05h_b6ujVQeAK8nsImul10F9aSov-rG3Hdn8NXe2Dai51zGvHc2C-9bQUBooF38&usqp=CAU",
+  clothing_id: 3,
+})
+image = Image.create({
+  url: "https://media.istockphoto.com/id/1347667305/photo/blank-sweatshirt-mock-up-in-front-and-back-views.webp?b=1&s=170667a&w=0&k=20&c=NZW8dm1yB2iZMmapyNG3qT1d-lokMwpHXHVBkRJmnhg=",
+  clothing_id: 4,
 })
